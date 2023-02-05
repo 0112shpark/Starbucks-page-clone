966e328c5dd9d2035d3e7442b1438958af99fb4e
0112shpark <000112shpark@naver.com>
2023-02-05 16:55:06 +0900

    :sparkle:add side badges

--- javascript/main.js
+++ javascript/main.js
@@ -17,3 +17,25 @@ searchInputEl.addEventListener("blur", function () {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+// badge elimination
+// _.throttle(function, time); ->delay
+const badgeEl = document.querySelector("header .badges");
+
+window.addEventListener(
+  "scroll",
+  _.throttle(function () {
+    if (window.scrollY > 500) {
+      // hide badge
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
